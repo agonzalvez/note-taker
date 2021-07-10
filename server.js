960b80fe90eea382a6e975de5a1092325b663f38
Alejandro Gonzalvez <agonzalvez1@gmail.com>
2021-07-10 15:26:12 -0400

    Modified server js as code was not condusive to server starting. Added routes to a separate folder.

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const PORT = process.env.PORT || 3001;
 const fs = require('fs');
 
 
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static('public'));
+app.use('/api',apiRoutes);
+app.use('/', htmlRoutes);
 
 
 
