6a1adc4316583156d2e4986721352c06206655e1
Alejandro Gonzalvez <agonzalvez1@gmail.com>
2021-07-10 15:32:31 -0400

    Corrected typo on indexRoutes and on server.js. Installed new package to generate a new ID when a new note is posted

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express');
-const path = require('path')
 const app = express();
 const PORT = process.env.PORT || 3001;
+const apiRoutes = require('./routes/apiRoutes');
+const indexRoutes = require('./routes/indexRoutes');
 
-const fs = require('fs');
+// const fs = require('fs');
 
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 app.use('/api',apiRoutes);
-app.use('/', htmlRoutes);
+app.use('/', indexRoutes);
 
 
 
