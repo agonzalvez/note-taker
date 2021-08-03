785ddeb1330afe981b035fd03ebe78ca80301b84
Alejandro Gonzalvez <alejandrogonzalvez@Alejandros-MacBook-Pro.local>
2021-08-02 22:39:16 -0400

    Deleted routes folder, and added routes directly to server.

--- server.js
+++ server.js
@@ -1,20 +1,56 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+
 const app = express();
 const PORT = process.env.PORT || 3001;
-const apiRoutes = require('./routes/apiRoutes');
-const indexRoutes = require('./routes/indexRoutes');
-
-// const fs = require('fs');
 
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
-app.use('/api',apiRoutes);
-app.use('/', indexRoutes);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
 
-
-app.listen(PORT, () => {
-    console.log(`API server now on port 3001!`);
+app.post('/api/notes', (req, res) => {
+    var notes;
+    var note = req.body;
+    fs.readFile('db/db.json', 'utf8', (error, data) => {
+        error ? console.error(error) : notes = JSON.parse(data);
+        //console.log(notes);
+        notes.push(note);
+        fs.writeFile('db/db.json', JSON.stringify(notes), (err) =>
+            err ? console.error(err) : res.json(notes)
+        );
+    }
+    );
 });
+
+app.get('/api/notes', (req, res) => {
+    var notes;
+    fs.readFile('db/db.json', 'utf8', (error, data) => {
+        error ? console.error(error) : notes = JSON.parse(data);
+        console.log(notes);
+        res.json(notes);
+    }
+    );
+});
+
+
+app.delete('/api/notes/:id', (req, res) => {
+    var notes;
+    fs.readFile('db/db.json', 'utf8', (error, data) => {
+        error ? console.error(error) : notes = JSON.parse(data);
+        //console.log(notes);
+        notes.splice(req.params.id, 1);
+        fs.writeFile('db/db.json', JSON.stringify(notes), (err) =>
+            err ? console.error(err) : res.json(notes)
+        );
+    }
+    );
+});
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
